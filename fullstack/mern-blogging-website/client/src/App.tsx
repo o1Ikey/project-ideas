65ab9fa71d9a114fb7ec21b1dc9ba5f61e22d266
Huy Truong (FE Vietnam) <huy.truong@enouvo.com>
2024-02-29 00:26:12 +0700

    FE: user auth form

--- fullstack/mern-blogging-website/client/src/App.tsx
+++ fullstack/mern-blogging-website/client/src/App.tsx
@@ -1,12 +1,13 @@
 import { Navbar } from '@/components/navbar'
 import { Route, Routes } from 'react-router-dom'
+import { UserAuthForm } from './pages/userAuthForm'
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navbar/>}>
-        <Route path="/signin" element={<h1>h2</h1>}/>
-        <Route path="/signup" element={<h1>h2</h1>}/>
+        <Route path="/signin" element={<UserAuthForm type='sign-in'/>}/>
+        <Route path="/signup" element={<UserAuthForm type='sign-up'/>}/>
       </Route>
     </Routes>
 
---
+++ fullstack/mern-blogging-website/client/src/components/input.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from 'react'
+
+type IInputBox = JSX.IntrinsicElements['input'] & {
+    name:string;
+    placeholder:string
+    id?:string;
+    defaultValue?:string
+    icon:ReactNode
+}
+
+export const InputBox = (props:IInputBox) => {
+  return <div className="relative w-[100%] mb-4">
+    <input {...props} className="input-box" />
+    {props.icon}
+  </div>
+}
