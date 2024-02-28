cfb64d47ec484c53b3d9fc64b5ef37a856eb409f
Huy Truong (FE Vietnam) <huy.truong@enouvo.com>
2024-02-28 22:49:01 +0700

    FE: setting up routes

--- fullstack/mern-blogging-website/client/src/App.tsx
+++ fullstack/mern-blogging-website/client/src/App.tsx
@@ -1,8 +1,15 @@
 import { Navbar } from '@/components/navbar'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
-    <Navbar/>
+    <Routes>
+      <Route path="/" element={<Navbar/>}>
+        <Route path="/signin" element={<h1>h2</h1>}/>
+        <Route path="/signup" element={<h1>h2</h1>}/>
+      </Route>
+    </Routes>
+
   )
 }
 
